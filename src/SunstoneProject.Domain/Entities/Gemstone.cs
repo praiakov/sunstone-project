11f9f33be62a9f17eaea96c72df69f4e6988065e
praiakov <adrianopraia@live.com>
2021-08-03 21:21:48 -0300

    feat(domain): add entities, enums

---
+++ src/SunstoneProject.Domain/Entities/Gemstone.cs
@@ -0,0 +1,12 @@
+﻿using SunstoneProject.Domain.Enums;
+
+namespace SunstoneProject.Domain.Entities
+{
+    public class Gemstone
+    {
+        public string Name { get; private set; }
+        public decimal Carat { get; private set; }
+        public decimal Clarity { get; private set; }
+        public Colors Color { get; private set; }
+    }
+}
---
+++ src/SunstoneProject.Domain/Enums/Colors.cs
@@ -0,0 +1,20 @@
+﻿namespace SunstoneProject.Domain.Enums
+{
+    public enum Colors
+    {
+        Red,
+        Black,
+        BiColor,
+        Blue,
+        Brown,
+        Colourless,
+        Green,
+        Yellow,
+        Gray,
+        Purple,
+        White,
+        Multicolor,
+        Pink,
+        Orange
+    }
+}
