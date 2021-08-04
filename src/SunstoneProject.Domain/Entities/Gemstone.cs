7cbec77aa9038df447f0f249915ce36c641a8a9e
praiakov <adrianopraia@live.com>
2021-08-03 23:09:21 -0300

    test(domain): add gemstone test

--- src/SunstoneProject.Domain/Entities/Gemstone.cs
+++ src/SunstoneProject.Domain/Entities/Gemstone.cs
@@ -9,5 +9,13 @@ namespace SunstoneProject.Domain.Entities
         public decimal Carat { get; private set; }
         public decimal Clarity { get; private set; }
         public Colors Color { get; private set; }
+
+        public Gemstone(string name, decimal carat, decimal clarity, Colors color)
+        {
+            Name = name;
+            Carat = carat;
+            Clarity = clarity;
+            Color = color;
+        }
     }
 }
---
+++ tests/SunstoneProject.Tests/Domain/GemstoneTest.cs
@@ -0,0 +1,19 @@
+﻿using SunstoneProject.Domain.Entities;
+using Xunit;
+using SunstoneProject.Domain.Enums;
+
+namespace SunstoneProject.Tests.Domain
+{
+    public class GemstoneTest
+    {
+        [Fact]
+        public void CreateGemstone_WhenPayloadBeOk_ReturnDomainOK()
+        {
+            // arrange & act
+            var gemstone = new Gemstone("Amethyst Cabochon", 12, 89, Colors.Purple);
+
+            // assert
+            Assert.NotNull(gemstone);
+        }
+    }
+}
