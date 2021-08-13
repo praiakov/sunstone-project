82a74ff6bbefe0c26d50f8e7e049823189fb2c08
praiakov <adrianopraia@live.com>
2021-08-12 21:02:58 -0300

    feat(application): add gemstone usecase

---
+++ src/SunstoneProject.Api/Controllers/V1/Gemstone/GemstoneController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SunstoneProject.Application.UseCases.GemstoneUseCase;
+using System.Threading.Tasks;
+
+namespace SunstoneProject.Api.Controllers.V1.Gemstone
+{
+    [Route("/api/v1/gemstone")]
+    [ApiController]
+    public class GemstoneController : ControllerBase
+    {
+        private readonly ILogger<GemstoneController> _logger;
+
+        public GemstoneController(ILogger<GemstoneController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        public async Task<IActionResult> Post([FromServices] IGemstoneUseCase gemstoneUseCase)
+        {
+            _logger.LogInformation("CheckoutController.Post");
+
+            await gemstoneUseCase
+                .ExecuteAsync(null);
+
+            return Accepted();
+        }
+    }
+}
---
+++ src/SunstoneProject.Application/UseCases/GemstoneUseCase/IGemstoneUseCase.cs
@@ -0,0 +1,10 @@
+﻿using SunstoneProject.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace SunstoneProject.Application.UseCases.GemstoneUseCase
+{
+    public interface IGemstoneUseCase
+    {
+        Task ExecuteAsync(Gemstone gemstone);
+    }
+}
