b90df7e3308c5f313c504433b16e9dd6b3c11c9c
praiakov <adrianopraia@live.com>
2021-08-14 19:40:19 -0300

    refactor(api) : gemstone usecase

--- src/SunstoneProject.Api/Controllers/V1/Gemstone/GemstoneController.cs
+++ src/SunstoneProject.Api/Controllers/V1/Gemstone/GemstoneController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SunstoneProject.Api.Controllers.V1.InputModels;
 using SunstoneProject.Application.UseCases.GemstoneUseCase;
 using System.Threading.Tasks;
 
@@ -17,16 +18,28 @@ namespace SunstoneProject.Api.Controllers.V1.Gemstone
             _logger = logger;
         }
 
+        /// <summary>
+        /// Create Gemstone
+        /// </summary>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status202Accepted)]
-        public async Task<IActionResult> Post([FromServices] IGemstoneUseCase gemstoneUseCase)
+        public async Task<IActionResult> Post(GemstoneInputModel gemstoneInputModel,[FromServices] IGemstoneUseCase gemstoneUseCase)
         {
             _logger.LogInformation("CheckoutController.Post");
 
             await gemstoneUseCase
-                .ExecuteAsync(null);
+                .ExecuteAsync(Mapping(gemstoneInputModel));
 
             return Accepted();
         }
+
+
+        #region Private methods
+        private static Domain.Entities.Gemstone Mapping(GemstoneInputModel gemstoneInputModel)
+        {
+            return new Domain.Entities.Gemstone(gemstoneInputModel.Name, gemstoneInputModel.Carat, 
+                                                gemstoneInputModel.Clarity, gemstoneInputModel.Color);
+        }
+        #endregion
     }
 }
---
+++ src/SunstoneProject.Api/Controllers/V1/InputModels/GemstoneInputModel.cs
@@ -0,0 +1,30 @@
+﻿using SunstoneProject.Domain.Enums;
+
+namespace SunstoneProject.Api.Controllers.V1.InputModels
+{
+    /// <summary>
+    /// Corresponds gemstone
+    /// </summary>
+    public class GemstoneInputModel
+    {
+        /// <summary>
+        /// Gemstone's name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gemstone's carat
+        /// </summary>
+        public decimal Carat { get; set; }
+
+        /// <summary>
+        /// Gemstone's clarity
+        /// </summary>
+        public decimal Clarity { get; set; }
+
+        /// <summary>
+        /// Gemstone's color
+        /// </summary>
+        public Colors Color { get; private set; }
+    }
+}
