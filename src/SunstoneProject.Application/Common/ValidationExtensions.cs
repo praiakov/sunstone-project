cc329f78d1abbed09a85401d2851f4ba36909fbe
praiakov <adrianopraia@live.com>
2022-01-20 21:10:57 -0300

    feat(filter): add custom filter

---
+++ src/SunstoneProject.Application/Common/ValidationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SunstoneProject.Application.Common
+{
+    public static class ValidationExtensions
+    {
+        public static bool IsNotEmptyOrNull(this string value) => !string.IsNullOrWhiteSpace(value);
+
+        public static bool IsValidGuid(this Guid guid) => guid != Guid.Empty;
+
+        public static bool IsGreaterThan(this string value, int length) => !string.IsNullOrWhiteSpace(value) && value.Length > length;
+    }
+}
---
+++ src/SunstoneProject.Domain/Exceptions/CustomErrorException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SunstoneProject.Domain.Exceptions
+{
+    public class CustomErrorException : Exception
+    {
+        public string Code { get; set; }
+
+        public CustomErrorException(string code, string message)
+            : base(message)
+        {
+            Code = code;
+        }
+
+    }
+}
