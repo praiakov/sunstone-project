17032533ad779a924bc009ecb09aeb67ad7c18be
praiakov <adrianopraia@live.com>
2022-02-06 14:01:20 -0300

    feat(swagger): add the Swagger UI at the app's root

--- src/SunstoneProject.Api/Startup.cs
+++ src/SunstoneProject.Api/Startup.cs
@@ -70,7 +70,11 @@ namespace SunstoneProject.Api
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SunstoneProject.Api v1"));
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "SunstoneProject.Api v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseRouting();
