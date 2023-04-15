539bf74f2a3f96d0c98b13946fb170e5517d429f
praiakov <adrianopraia@live.com>
2023-04-15 16:11:01 -0300

    feat(sql): add seed

---
+++ dbscripts/seed.sql
@@ -0,0 +1,15 @@
+﻿
+\c sunstonedb
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE "Gemstones"
+(
+ "Id" uuid DEFAULT uuid_generate_v4 (),
+ "Name" VARCHAR (30) NOT NULL,
+ "Carat" decimal NOT NULL,
+ "Clarity" decimal NOT NULL,
+ "Color" integer NOT NULL,
+ "Created" date NOT NULL
+);
+
